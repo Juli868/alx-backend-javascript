163a14a12a370e4f9750cfe246b381c166b71737
juli868 <bugijul@gmail.com>
2024-03-04 14:22:08 +0200

    Repo activation

---
+++ 0x00-ES6_basic/5-main.js
@@ -0,0 +1,5 @@
+bob@dylan:~$ cat 5-main.js
+import concatArrays from './5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
+
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,1 @@
+After a long period without js now is the time to go deeper. In this repo is the foundations of ES6(Ecma script 6). Wish you the best.
