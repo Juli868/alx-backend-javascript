2bd4d6cbd1bd576db97ef7ec616f1219a5d03182
juli868 <bugijul@gmail.com>
2024-03-13 00:33:15 +0200

    Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,6 @@
 export default function getStudentIdsSum(students) {
-  let result = 0;
-  for (let i = 0; i < students.length; i += 1) {
-    result += students[i].id;
+  function add(sum, student) {
+    return sum + student.id;
   }
-  return result;
+  return students.reduce(add, 0);
 }
