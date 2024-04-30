26f967e4086e00f6fc011053f035d66a199a676a
juli868 <bugijul@2001>
2024-04-30 22:57:58 +0200

    Edit on the server creation.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello Holberton School!');
 });
 server.listen(1245, () => {
-  console.log('...');
+    console.log('...');
 });
